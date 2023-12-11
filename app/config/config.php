0b619a882f2ae1b3a9d684e2d049159bceb58563
bala0008 <balakrish0008@gmail.com>
2023-12-11 17:27:59 +0530

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+//Database params
+define('DB_HOST', 'localhost'); //Add your db host
+define('DB_USER', 'postgres'); // Add your DB root
+define('DB_PASS', 'postgres'); //Add your DB pass
+define('DB_PORT', '5432'); //Add your Port pass
+define('DB_NAME', 'storeprocedure'); //Add your DB Name
+define('SALT', 'tnsta');
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://10.163.19.146:83/storeprocedure/');
+define('FILEROOT', 'C:/tnsta/');
+define('SITENAME', 'MVC Frame');
+define('', '');
